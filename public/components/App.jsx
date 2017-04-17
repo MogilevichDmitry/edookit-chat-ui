2ed73a6c19f917a64b4d415f972f92d9b74c952b
Dmitry Mogilevich <dmitry.mogilevich@devexperts.com>
2017-04-18 00:39:49 +0300

    Configure webpack

---
+++ public/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './main.less';
+
+export default class App extends React.Component {
+  render() {
+    return (
+     <div>
+        <h1>Hello Worldss</h1>
+      </div>
+    );
+  }
+}
