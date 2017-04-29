f79b03e20406e0afce3dfbdd22fcad3ecf8a6f1c
Dmitry Mogilevich <dmitry.mogilevich@devexperts.com>
2017-04-29 10:17:55 +0300

    Fix routes

--- public/routes.js
+++ public/routes.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { IndexRoute, Route } from 'react-router';
 import App from './components/App.jsx';
 import Chat from './components/Chat/Chat.jsx';
-import Login from './components/Login/Login.jsx';
 
 export default () => {
   return <Route>
     <Route path="/" component={App}>
-      <Route path="/chat" component={Chat}></Route>
-      <Route path="/login" component={Login}></Route>
+      <IndexRoute path="/chat" component={Chat} />
     </Route>
   </Route>
 }
