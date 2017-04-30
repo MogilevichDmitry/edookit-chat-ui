ee8d88e7e46e2d876312cce912fe2430b3f2ecb8
Dmitry Mogilevich <dmitry.mogilevich@devexperts.com>
2017-05-01 00:29:51 +0300

    Separate repos, Remove Login page

--- public/routes.js
+++ public/routes.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { IndexRoute, Route } from 'react-router';
 import App from './components/App.jsx';
-import Login from './components/Login/Login.jsx';
 import Chat from './components/Chat/Chat.jsx';
 
 export default () => {
   return <Route>
     <Route path="/" component={App}>
       <IndexRoute component={Chat} />
-      <Route path="/login" component={Login} />
     </Route>
   </Route>
 }
